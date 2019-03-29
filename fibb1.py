d4b95a854db7306425bb6d902ba174aee8b649aa
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-29 15:33:54 -0400

    Finished fibonacci problem

--- fibb1.py
+++ fibb1.py
@@ -1,1 +1,20 @@
+def even_fibonacci():
+    fib_list = [1, 2]
+    new_value = 3
+    while new_value < 4000000:
+        fib_list.append(new_value)
+        new_value = fib_list[-1] + fib_list[-2]
+    fib_list.append(new_value)
+    even_fib_list = []
+    for i in range(0, len(fib_list)):
+        if i % 2 == 0:
+            even_fib_list.append(i)
+    sum = 0
+    for i in range(0, len(even_fib_list)):
+        sum += i
+    print(sum)
+    return sum
 
+
+if __name__ == "__main__":
+    even_fibonacci()
