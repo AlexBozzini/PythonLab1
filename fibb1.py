9524583ceda7004f89fa29293d0684a4738ee2c7
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-29 16:12:16 -0400

    Fixed fibonacci problem

--- fibb1.py
+++ fibb1.py
@@ -6,13 +6,12 @@ def even_fibonacci():
         new_value = fib_list[-1] + fib_list[-2]
     fib_list.append(new_value)
     even_fib_list = []
-    for i in range(0, len(fib_list)):
+    for i in fib_list:
         if i % 2 == 0:
             even_fib_list.append(i)
     sum = 0
-    for i in range(0, len(even_fib_list)):
+    for i in even_fib_list:
         sum += i
-    print(sum)
     return sum
 
 
